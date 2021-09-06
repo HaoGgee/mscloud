321255ab3b176d10021a1e1a15cc4708281d70c3
lxp <414991176@qq.com>
2021-09-06 10:17:07 +0800

    first commit mscloud practice

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients     //开启Feign使用
+public class DeptConsumer80_Feign_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_Feign_App.class,args);
+    }
+}
